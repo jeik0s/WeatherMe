241966a50e3d4933b9e1fe2878aec704c146dc87
Jakub Zaremba <jzaremba@MAgiC.local>
2021-09-12 22:18:24 +0200

    Get weather response for given coord

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weatherme/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -13,6 +14,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
   await location.getCurrentLocation();
   print(location.latitude);
   print(location.longitude);
+//  getData(location.latitude, location.longitude);
+  }
+
+  void getData(double lat, double lon) async{
+    var url = Uri.https('api.openweathermap.org', '/data/2.5/weather',{'lat':lat.toString(), 'lon':lon.toString(), 'appid': '4f678fe256273c7c0f0930953f287ba6'});
+    print(url);
+    var response = await http.get(url);
+    if(response.statusCode == 200){
+      print(response.body);
+    }
   }
 
 
@@ -24,6 +35,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    getData(37.33233141, -122.0312186);
+    return Scaffold(
+      body: Container(
+        width: 200,
+        height: 200,
+        color: Colors.lightGreen,
+        child: FlatButton(
+          onPressed: (){
+            getData(37.33233141, -122.0312186);
+          },
+        ),
+      ),
+    );
   }
 }
+
+
+//4f678fe256273c7c0f0930953f287ba6
