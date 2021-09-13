c4c7adf9261ce8b3fee8e61b5c78c16b004f9b98
Jakub Zaremba <jzaremba@MAgiC.local>
2021-09-13 20:24:55 +0200

    New lib flutter-spinkit, adds loader to the screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:weatherme/screens/location_screen.dart';
 import 'package:weatherme/services/location.dart';
 import 'package:weatherme/services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,38 +10,37 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
-
   void getLocationData() async {
-  Location location = Location();
-  await location.getCurrentLocation();
-  NetworkHelper networkHelper = NetworkHelper(latitude: location.latitude, longitude: location.longitude);
-  var weatherData = await networkHelper.getData();
-  print(weatherData);
+    Location location = Location();
+    await location.getCurrentLocation();
+    NetworkHelper networkHelper = NetworkHelper(
+        latitude: location.latitude, longitude: location.longitude);
+    var weatherData = await networkHelper.getData();
+    print(weatherData);
+
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return LocationScreen();
+    }));
   }
 
   @override
-  void initState(){
-
+  void initState() {
     super.initState();
+    getLocationData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: 200,
-        height: 200,
-        color: Colors.lightGreen,
-        child: FlatButton(
-          onPressed: (){
-            getLocationData();
-          },
-        ),
+      body: Center(
+          child:
+          SpinKitDoubleBounce(
+            color: Colors.white,
+            size: 100.0,
+          )
       ),
     );
   }
 }
-
 
 //4f678fe256273c7c0f0930953f287ba6
